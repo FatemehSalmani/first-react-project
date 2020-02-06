f85e7104a5cdcbd468b3de01360a9235e50088e8
faslm <fatemeh.salmani.4033@gmail.com>
2020-02-06 02:23:16 -0800

    Updates

---
+++ precache-manifest.a08dbb917a75523ea6c5dc3e7cc8a586.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf4e63e628c18fb642d03e089725dd14",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "92b15dd4aab9d52f897e",
+    "url": "/first-react-project/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "e1d2a8dc6f330c1e2e4c",
+    "url": "/first-react-project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "92b15dd4aab9d52f897e",
+    "url": "/first-react-project/static/js/2.f981af1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/first-react-project/static/js/2.f981af1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1d2a8dc6f330c1e2e4c",
+    "url": "/first-react-project/static/js/main.730e59f7.chunk.js"
+  },
+  {
+    "revision": "73a9f3cd887f57ee9646",
+    "url": "/first-react-project/static/js/runtime-main.00117250.js"
+  }
+]);
