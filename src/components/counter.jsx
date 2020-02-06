4ba210f259b85e7b5195520aaa7b04fb5f669680
faslm <fatemeh.salmani.4033@gmail.com>
2020-02-06 02:10:01 -0800

    Added git page

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,22 +7,22 @@ class Counter extends Component {
         this.setState({value:this.state.value+1});
     };*/
 
-    render() { 
+    render() {
         return (
-        <div>
-            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-            <button 
-            onClick={this.props.onIncrement(this.props.counter)}
-            className="btn btn-secondary btn-sm">
-                Increment
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button
+                    onClick={() => { this.props.onIncrement(this.props.counter) }}
+                    className="btn btn-secondary btn-sm">
+                    Increment
             </button>
-            <button
-            onClick={() => this.props.onDelete(this.props.counter.id)} 
-            className="btn btn-danger btn-sm m-2">
-                Delete
+                <button
+                    onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm m-2">
+                    Delete
             </button>
             </div>
-            );
+        );
     }
     getBadgeClasses() {
         let classes = "badge m-2 badge-";
@@ -31,9 +31,9 @@ class Counter extends Component {
     }
 
     formatCount() {
-        const {value} = this.props.counter;
-        return value ===0 ? "Zero" : value;
+        const { value } = this.props.counter;
+        return value === 0 ? "Zero" : value;
     }
 }
- 
+
 export default Counter;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,23 +1,27 @@
 import React, { Component } from 'react';
 import Counter from './counter'
 class counters extends Component {
-    
-    render() { 
-        return ( 
+    constructor(props) {
+        super(props)
+        this.state = props
+    }
+    render() {
+        const { onReset, Counters, onDelete, onIncrement } = this.props
+        return (
             <div>
-                <button 
-                onClick={this.props.onReset}
-                className="btn btn-primary btn-sm m-2">Reset</button>
-                {this.props.Counters.map(counter => 
-                <Counter
-                 key={counter.id} 
-                 counter = {counter}
-                 onDelete = {this.props.onDelete}
-                 onIncrement = {this.props.onIncrement}>
-                </Counter>)}
+                <button
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2">Reset</button>
+                {Counters.map(counter =>
+                    <Counter
+                        key={counter.id}
+                        counter={counter}
+                        onDelete={onDelete}
+                        onIncrement={onIncrement}>
+                    </Counter>)}
             </div>
-         );
+        );
     }
 }
- 
+
 export default counters;
