d8f0c7ee0ad06d4ea36be01e28d9265dd59de914
Fatemeh <fatima27579@gmail.com>
2020-02-07 23:50:10 -0800

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,10 +6,12 @@ class navbar extends Component {
             <nav className="navbar navbar-light bg-light">
              <span className="navbar-brand" >
                  updete navbar 
-             </span>
+             
                 <span className="badge badge-pill badge-secondary">
                      {this.props.totalCounters}
                  </span>
+                 
+            </span>
             </nav>
             );
     }
